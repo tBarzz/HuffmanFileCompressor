6bafb069068d65763160e3f394d51b4e72bcdf9b
tBarzz <tejasbharadwaj77@gmail.com>
2026-09-14 20:10:00 +0530

    Backend to browser file transmission improved

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,13 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const cppExecutable = path.resolve(__dirname, "..","build", "HuffmanFileCompressor.exe");
 
+function uploadsCleaner(requestDir)
+{
+    fs.rm(requestDir, {recursice : true, force : true}, (error) => {
+        if(error) console.error(error);
+    });
+}
+
 const storage = multer.diskStorage({
     destination : (req,file,cb) => {
         const requestId = randomUUID();
@@ -35,27 +42,43 @@ app.get("/", (req,res) => {
 
 app.post("/compress", upload.single("file"), (req,res) => {
     const inputFilePath = req.file.path;
+    const requestDir = path.dirname(inputFilePath);
     execFile(cppExecutable, [1, inputFilePath], (error, stdout) => {
         if(error){
             console.error(error);
+            uploadsCleaner(requestDir);
             return res.status(500).send("Compression Failed.");
         }
         const compressedFilePath = stdout.trim();
         console.log("C++ compression completed.");
-        res.download(compressedFilePath);
+        res.download(compressedFilePath, (error) => {
+            if(error){
+                console.error(error);
+                res.status(500).send("File transmission problem.");
+            } 
+            uploadsCleaner(requestDir);
+        });
     })
 });
 
 app.post("/decompress", upload.single("file"), (req,res) => {
     const inputFilePath = req.file.path;
+    const requestDir = path.dirname(inputFilePath);
     execFile(cppExecutable, [2 ,inputFilePath], (error, stdout) => {
         if(error){
             console.error(error);
+            uploadsCleaner(requestDir);
             return res.status(500).send("Decompression Failed.");
         }
         const decompFilePath = stdout.trim();
         console.log("C++ decompression completed.");
-        res.download(decompFilePath);
+        res.download(decompFilePath, (error) => {
+            if(error){
+                console.error(error);
+                res.status(500).send("File transmission problem.");
+            }
+            uploadsCleaner(requestDir);
+        });
     })
 });
 
