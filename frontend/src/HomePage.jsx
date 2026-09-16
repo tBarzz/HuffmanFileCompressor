de4e4961f9aa13b7e0eb6ecfb546248a45aff97c
tBarzz <tejasbharadwaj77@gmail.com>
2026-09-16 23:22:39 +0530

    basic frontend UI Part-1

---
+++ frontend/src/HomePage.jsx
@@ -0,0 +1,11 @@
+import FileDropArea from "./components/FileDropArea";
+
+function HomePage(){
+    return(
+        <div className = "DropBox">
+            <FileDropArea/>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ frontend/src/components/FileDropArea.jsx
@@ -0,0 +1,13 @@
+function FileDropArea(){
+    return(
+        <div className = "fileDropArea">
+            <button>UPLOAD</button>
+
+            <div className = "collectionBox">
+                <p>Drag and drop files here.</p>
+            </div>
+        </div>
+    );
+}
+
+export default FileDropArea;
