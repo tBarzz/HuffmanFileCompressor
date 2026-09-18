5ae7d505596d9b7192282e79434ae7a3932f6011
tBarzz <tejasbharadwaj77@gmail.com>
2026-09-18 14:39:55 +0530

    Implemented file selection on frontend

--- frontend/src/components/FileDropArea.jsx
+++ frontend/src/components/FileDropArea.jsx
@@ -1,10 +1,32 @@
+import { useRef } from "react";
+import { useState } from "react";
+
 function FileDropArea(){
+    const fileInputRef = useRef(null);
+    const [selectedFile, setSelectedFile] = useState(null);
     return(
         <div className = "fileDropArea">
-            <button>UPLOAD</button>
 
-            <div className = "collectionBox">
-                <p>Drag and drop files here.</p>
+            <input
+                type = "file"
+                ref = {fileInputRef}
+                onChange={(event) => setSelectedFile(event.target.files[0])}
+            />
+
+            <button onClick={() => fileInputRef.current.click()}>
+                UPLOAD
+            </button>
+
+            <div 
+                className = "collectionBox"
+                onDragOver = {(event) => event.preventDefault()}
+                onDrop = {(event) => {
+                    event.preventDefault();
+                    setSelectedFile(event.dataTransfer.files[0]);
+                }}>
+                <p className = { selectedFile ? "selectedFile" : "placeHolder" }> 
+                    { selectedFile ? selectedFile.name : "Drag and drop files here." } 
+                </p>
             </div>
         </div>
     );
