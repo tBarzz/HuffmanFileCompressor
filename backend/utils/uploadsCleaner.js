4c53889b623231e422481e82b19ae87b6a4b9bcd
tBarzz <tejasbharadwaj77@gmail.com>
2026-09-20 17:08:36 +0530

    Uploads Cleaner error fixed

--- backend/utils/uploadsCleaner.js
+++ backend/utils/uploadsCleaner.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 function uploadsCleaner(requestDir)
 {
-    fs.rm(requestDir, {recursice : true, force : true}, (error) => {
+    fs.rm(requestDir, {recursive : true, force : true}, (error) => {
         if(error) console.error("Failed to clean requested directory: ", error);
     });
 }
